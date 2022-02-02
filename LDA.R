81b1ca6d561adbcd02571a0c935bf9045b3493de
Francesc0rtu <francescortu@live.it>
2022-02-02 19:27:31 +0100

    add first LDA experiment

---
+++ LDA.R
@@ -0,0 +1,21 @@
+
+#Try to do some LDA analysis of topics:
+  
+
+
+library(topicmodels)
+
+#create corpus object from the dataframe
+Corpus_obj <- Corpus(VectorSource(corpus$token))
+
+#Compute Document term matrix from the corpus object
+TDM <- DocumentTermMatrix(Corpus_obj, control = list(bounds = list(global = c(5, Inf))))
+
+#Compute the LDA
+topicModel <- LDA(TDM, 5, method = "VEM")
+
+#Print the first 10 words for each topics
+terms(topicModel, 17)
+
+#
+
