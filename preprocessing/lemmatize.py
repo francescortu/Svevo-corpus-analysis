3f696816275a9780afc5e9805d650936c4090486
erika.lena@outlook.it <erika.lena@outlook.it>
2022-02-03 19:10:38 +0100

    folder organization

---
+++ preprocessing/lemmatize.py
@@ -0,0 +1,36 @@
+import pandas
+import pandas as pd
+import numpy
+import spacy
+import sys
+
+def lemmatize():
+  sett = pd.read_csv("../csv/cleaned_svevo_dataset.csv")
+  sett["doc"] =""
+  sett["lemmatized_tokens"] = ""
+
+
+  nlp_ita = spacy.load("it_core_news_sm")
+  nlp_eng = spacy.load("en_core_web_sm")
+  nlp_fr = spacy.load("fr_core_news_sm")
+  nlp_de = spacy.load("de_core_news_sm")
+
+  for x in range(sett.shape[0]):
+      if sett["mainLanguage"][x] == "ITA" :
+          sett["doc"][x] = nlp_ita(sett["tokens"][x])
+      if sett["mainLanguage"][x] == "ENG" :
+          sett["doc"][x] = nlp_eng(sett["tokens"][x])
+      if sett["mainLanguage"][x] == "FRE" :
+          sett["doc"][x] = nlp_fr(sett["tokens"][x])
+      if sett["mainLanguage"][x] == "GER" :
+          sett["doc"][x] = nlp_de(sett["tokens"][x])
+
+
+  for i in range(sett.shape[0]):
+      sett["lemmatized_tokens"][i] = " ".join(token.lemma_ for token in sett["doc"][i])
+
+  sett.to_csv("../csv/cleaned_svevo_dataset.csv")
+
+
+if __name__ == "__main__":
+    lemmatize()
---
+++ preprocessing/preprocess.R
@@ -0,0 +1,61 @@
+library(tm)
+library(dplyr)
+library(tidytext)
+library(textstem)
+library(data.table)
+
+
+# the following function performs all the steps needed to preprocess the whole corpus
+#adjust columns, lower text, remove punctuation and numbers, then remove stop-words
+clean_text <- function(corpus) {
+  
+  corpus$n <- corpus$n + 1
+  names(corpus)[names(corpus) == 'n'] <- 'letter_number'
+  
+  names(corpus)[names(corpus) == 'corpus'] <- 'pair'
+  corpus$pair <- gsub("Schmitz","Svevo", corpus$pair)
+  
+  corpus$tokens <- NA
+  
+  sw_list_ita <- scan("stopwords_ita.txt", what="", sep="\n")
+  sw_list_eng <- scan("stopwords_eng.txt", what="", sep="\n")
+  
+  for(i in 1:nrow(corpus)) {
+    #remove punctuation
+    corpus$tokens[i] <- gsub("[^a-zA-Z]+"," ", tolower(corpus$text[i]))
+    #remove words shorter than 3 chars
+    corpus$tokens[i] <- gsub('\\b\\w{1,2}\\b',"", tolower(corpus$tokens[i]))
+
+    #remove stop words
+    if (grepl("ENG",corpus$mainLanguage[i])) {
+      lang = "english"
+      corpus$tokens[i] <- removeWords(corpus$tokens[i], c(stopwords(lang),  sw_list_eng))
+    }
+    if (grepl("ITA",corpus$mainLanguage[i])) {
+      lang = "italian"
+      corpus$tokens[i] <- removeWords(corpus$tokens[i], c(stopwords(lang), sw_list_ita))
+    }
+    if (grepl("FRE",corpus$mainLanguage[i])) {
+      lang = "french"
+      corpus$tokens[i] <- removeWords(corpus$tokens[i], stopwords(lang))
+    }
+    if (grepl("GER",corpus$mainLanguage[i])) {
+      lang = "german"
+      corpus$tokens[i] <- removeWords(corpus$tokens[i], stopwords(lang))
+    }
+
+
+  }
+
+  return(corpus)
+}
+
+# read corpus
+corpus <- read.csv("../csv/svevo_letters.csv", sep=";", encoding = "UTF-8")
+
+# clean corpus and save csv
+corpus <- clean_text(corpus)
+fwrite(corpus, paste0("../csv/cleaned_svevo_dataset.csv"),col.names = TRUE)
+
+# call python script to lemmatize tokens wrt the language used in each letter
+system('python lemmatize.py')
