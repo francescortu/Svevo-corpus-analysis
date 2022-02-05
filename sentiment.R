54c1fab410cf3e96c96c64f961cb58326a803ffd
BernardoManfriani <bernardomanfriani@gmail.com>
2022-02-05 12:45:02 +0100

    Plots and model

--- sentiment.R
+++ sentiment.R
@@ -1,3 +1,4 @@
+
 library(tidytext)
 
 corpus <- read.csv("csv/cleaned_svevo_dataset.csv", sep=",", encoding = "UTF-8")
@@ -20,11 +21,19 @@ for (x in corpus$tockens[7]){
 }
 
 library(wordcloud)
-wordcloud(corpus$tokens, scale = c(2, 1), min.freq = 100, colors = rainbow(30))
+wordcloud(corpus$tokens, scale = c(7, 1), min.freq = 1, colors = rainbow(30))
 
 
 model <- readRDS("../LDA/LDA_model_ita.rds")
 model
 
-plot(model)
+plot(model$hclust)
 
+set.seed(1234)
+wordcloud(model$top_terms, scale = c(2,1), min.freq = 1000000, colors = rainbow(20))
+wordcloud(words = model$top_terms, size = 1.6, scale=c(3.5,0.25), min.freq = 1,max.words=200,random.order=FALSE,rot.per=0.35,colors=brewer.pal(8,"Dark2"))
+
+
+library(wordcloud2)
+
+wordcloud2(data=model, size = 0.7, shape = 'pentagon')
