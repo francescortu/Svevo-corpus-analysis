3c7d2e0fe8145e75b4bab464c0094cf435c0185a
Francesc0rtu <francescortu@live.it>
2022-02-06 23:41:11 +0100

    removed files

--- sentiment.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from feel_it import EmotionClassifier, SentimentClassifier
-
-
-data = pd.read_csv("https://gist.githubusercontent.com/vinid/2286d0ae3d0e39153257b7b6607bf189/raw/0073d5c037dd1daf13991a44a148d957b885d9cd/italian_emotion_classification.csv")
-
-print(emotion_classifier.predict(data["sono molto felice"].values.tolist()))
