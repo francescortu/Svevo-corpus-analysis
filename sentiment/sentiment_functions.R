91ba127d3c4952350d85002212def2e8e9ba97b5
BernardoManfriani <bernardomanfriani@gmail.com>
2022-02-09 20:06:21 +0100

    TO-MERGE

--- sentiment/sentiment_functions.R
+++ sentiment/sentiment_functions.R
@@ -30,7 +30,7 @@ get_sentiment <- function(docs, corpus) {
   return(docs)
 }
 
-
+#TO-MERGE
 get_emotions_time <- function(dfEmotion){
   
   countYear <- dfEmotion %>% 
@@ -47,7 +47,7 @@ get_emotions_time <- function(dfEmotion){
   return(dfEmotion)
 }
 
-
+#TO-MERGE
 get_emotions_pair <- function(dfEmotion){
   
   countPair <- dfEmotion %>% 
