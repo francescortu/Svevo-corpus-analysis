86f4c66b8106e5a1640af18272aa9dc4751ff48f
Francesc0rtu <francescortu@live.it>
2022-02-12 15:51:51 +0100

    update

--- LDA/lda_functions.R
+++ LDA/lda_functions.R
@@ -117,7 +117,7 @@ one_model_analysis <- function(num_topics, corpus, save_results){
                    remove_numbers = FALSE, 
                    verbose = TRUE,
                    cpus = 4) # default is all available cpus on the system
-  set.seed(1254)
+  set.seed(12549)
   model <- FitLdaModel(dtm = dtm, 
                        k = num_topics,
                        iterations = 800, #  recommend at least 500 iterations or more
--- sentiment/sentiment_functions.R
+++ sentiment/sentiment_functions.R
@@ -4,8 +4,7 @@ library(tidyr)
 library(dplyr)
 library(lubridate)
 
-# this function returns a data frame which associates to each document 
-# a value for each sentiment (pos,neg,neu)
+
 get_sentiment <- function(docs, corpus) {
   sentiment_df <- read.csv("csv/pos_neg_neu.csv",  sep=",", encoding = "UTF-8") # read pre-classified set of words
   
@@ -31,97 +30,8 @@ get_sentiment <- function(docs, corpus) {
   return(docs)
 }
 
-# this function returns a data frame which associates to each sentiment 
-# a value for each topic in the model build using lda
-get_sentiment_topic <- function() {
-  # load lda model for topics
-  model <- readRDS("LDA/LDA_corpus_topic_model.rds")
-  
-  #phi gives P(token_v|topic_k)
-  k <- 5 # number of topics
-  n <- 1500 # number of chosen words
-  
-  tokens_topic <- GetTopTerms(phi = model$phi, M = n,return_matrix = TRUE)
-  tokens_topic <- melt(tokens_topic)[,-1]
-  colnames(tokens_topic) <- c("topic", "word")
-  tokens_topic$probability <- NA
-  tokens_topic$positive <- 0
-  tokens_topic$negative <- 0
-  tokens_topic$neutral <- 0
-  
-  sentiment_df <- read.csv("csv/pos_neg_neu.csv",  sep=",", encoding = "UTF-8") # read pre-classified set of words
-  
-  for(i in 1:k) {
-    for(j in 1:n) {
-      word <- tokens_topic$word[(i-1)*n + j]
-      tokens_topic$probability[(i-1)*n + j] <- model$phi[paste0("t_",i), word]
-      
-      if(nrow(sentiment_df[which(sentiment_df$word == word),])){
-        
-        corr <-  sentiment_df[which(sentiment_df$word == word),][1,]
-        
-        if(corr$sentiment == "positive") 
-          tokens_topic$positive[(i-1)*n + j] <- corr$polarity*tokens_topic$probability[(i-1)*n + j]
-        else if(corr$sentiment == "negative") 
-          tokens_topic$negative[(i-1)*n + j] <- corr$polarity*tokens_topic$probability[(i-1)*n + j]
-        else 
-          tokens_topic$neutral[(i-1)*n + j] <- corr$polarity*tokens_topic$probability[(i-1)*n + j]
-      }
-    }
-  }
-  
-  # sum sentiment wrt to topic
-  sentiment_topic <- tokens_topic %>%
-    group_by(topic) %>%
-    summarise(across(sentiment_labels, sum)) 
-  
-  sentiment_topic <- sentiment_topic  %>% mutate(n = negative/(negative+positive+neutral), p = positive/(negative+positive+neutral),
-                                               ne = neutral/(negative+positive+neutral))
-  
-  sentiment_topic$negative <- sentiment_topic$n
-  sentiment_topic$positive <- sentiment_topic$p
-  sentiment_topic$neutral <- sentiment_topic$ne
-  sentiment_topic <- sentiment_topic[, -((ncol(sentiment_topic) - 2):ncol(sentiment_topic))]
-  
-  sentiment_topic$topic <- c("salute", "famiglia", "libro", "pensieri", "viaggio" )
-  
-  return(sentiment_topic)
-}
-
-
-get_emotions_time <- function(dfEmotion){
-  
-  countYear <- dfEmotion %>% 
-    group_by(year) %>%
-    count()
-  
-  dfEmotion <- dfEmotion %>% 
-    group_by(year) %>%
-    summarise(across(everything(), sum))
-  
-  dfEmotion$countYear <- countYear$n
-  dfEmotion[,3:8] <- round(dfEmotion[,3:8]/dfEmotion$countYear, digits = 1)
-  
-  return(dfEmotion)
-}
-
-
-get_emotions_pair <- function(dfEmotion){
-  
-  countYear <- dfEmotion %>% 
-    group_by(year) %>%
-    count()
-  
-  dfEmotion <- dfEmotion %>% 
-    group_by(year) %>%
-    summarise(across(everything(), sum))
-  
-  dfEmotion$countYear <- countYear$n
-  dfEmotion[,3:8] <- round(dfEmotion[,3:8]/dfEmotion$countYear, digits = 1)
-  
-  return(dfEmotion)
-}
-
+############################################## TO-MERGE #########################################
+#############################################################################################
 get_emotions_time <- function(dfEmotion){
   
   countYear <- dfEmotion %>% 
@@ -155,5 +65,7 @@ get_emotions_pair <- function(dfEmotion){
   return(dfEmotion)
 }
 
+################################################################################################
+################################################################################################
 
 
